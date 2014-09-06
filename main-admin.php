907037482ff059810ba8c940fd63353f5c59e533
Sergey S. Betke <sergey.s.betke@yandex.ru>
2014-09-06 22:42:48 +0400

    * передача опциональных параметров массивами

--- main-admin.php
+++ main-admin.php
@@ -112,77 +112,55 @@ new WPF\Plugin\Part\Advanced (
 	
 	, new WPF\GUI\Setting\Page\PluginOptions(
 		new WPF\GUI\Setting\Page\Section\Base( 'rfc7234', __( 'RFC 7234 HTTP 1.1 Cache-Control headers options', TEXTDOMAIN )
-			, new WPF\GUI\Setting\Page\Control\Input(
-				'no_cache'
-				, NO_CACHE
-				, __( 'Disable cache for headers', TEXTDOMAIN )
-				, __( 'Disable cache for specified HTTP headers, but enable cache for other parts of response. <code>*</code> - fully disable server, client (browser) and proxy servers cache, <code>""</code> - don\'t disable cache. <a href="http://tools.ietf.org/html/rfc7234#section-5.2.2.2" target="_blank"><code>no-cache</code></a> parameter of <code>Cache-Control</code> header.', TEXTDOMAIN )
-			)
-			, new WPF\GUI\Setting\Page\Control\CheckBox(
-				'no_store'
-				, NO_STORE
-				, __( 'Disable permanent cache storage', TEXTDOMAIN )
-				, __( 'Disable permanent cache storage (for security reasons) - <a href="http://tools.ietf.org/html/rfc7234#section-5.2.2.3" target="_blank"><code>no-store</code></a> parameter of <code>Cache-Control</code> header.', TEXTDOMAIN )
-			)
-			, new WPF\GUI\Setting\Page\Control\CheckBox(
-				'public'
-				, CACHE_PUBLIC
-				, __( 'Enable public cache', TEXTDOMAIN )
-				, __( 'Enable public cache (client (browser) and proxy servers cache) - <a href="http://tools.ietf.org/html/rfc7234#section-5.2.2.5" target="_blank"><code>public</code></a> parameter of <code>Cache-Control</code> header.', TEXTDOMAIN )
-			)
-			, new WPF\GUI\Setting\Page\Control\Input(
-				'private'
-				, CACHE_PRIVATE
-				, __( 'Disable public cache for headers', TEXTDOMAIN )
-				, __( 'Disable public cache for specified HTTP headers, but don\'t disable public cache for other parts of response. <code>*</code> - fully disable public (proxy servers) cache, <code>""</code> - don\'t disable public cache. <a href="http://tools.ietf.org/html/rfc7234#section-5.2.2.6" target="_blank"><code>private</code></a> parameter of <code>Cache-Control</code> header.', TEXTDOMAIN )
-			)
-			, new WPF\GUI\Setting\Page\Control\Number(
-				'max_age'
-				, MAX_AGE
-				, __( 'Default <code>max-age</code> value', TEXTDOMAIN )
-				, __( 'Default <a href="http://tools.ietf.org/html/rfc7234#section-5.2.2.8" target="_blank"><code>max-age</code></a> value (max cache time-to-live, seconds) for html wordpress pages.', TEXTDOMAIN )
-				, __( 'seconds', TEXTDOMAIN )
-			)
-			, new WPF\GUI\Setting\Page\Control\CheckBox(
-				'must_revalidate'
-				, MUST_REVALIDATE
-				, __( 'Doesn\'t use stale cache', TEXTDOMAIN )
-				, __( 'If cache content to become stale, a cache <strong>must not</strong> use the response to satisfy requests without successful validation on the origin server. <a href="http://tools.ietf.org/html/rfc7234#section-5.2.2.1" target="_blank"><code>must-revalidate</code></a> parameter of <code>Cache-Control</code> header.', TEXTDOMAIN )
-			)
-			, new WPF\GUI\Setting\Page\Control\CheckBox(
-				'proxy_revalidate'
-				, PROXY_REVALIDATE
-				, __( 'Doesn\'t use stale cache on proxy servers', TEXTDOMAIN )
-				, __( 'If cache content on proxy servers to become stale, a cache <strong>must not</strong> use the response to satisfy requests without successful validation on the origin server. But private (browser) cache can use stale content. <a href="http://tools.ietf.org/html/rfc7234#section-5.2.2.7" target="_blank"><code>proxy-revalidate</code></a> parameter of <code>Cache-Control</code> header.', TEXTDOMAIN )
-			)
-			, new WPF\GUI\Setting\Page\Control\CheckBox(
-				'no_transform'
-				, NO_TRANSFORM
-				, __( 'Don\'t transform content parts in cache', TEXTDOMAIN )
-				, __( 'Cache (regardless of whether it implements a cache) <strong>must not</strong> transform the payload, as defined in <a href="http://tools.ietf.org/html/rfc7230#section-5.7.2" target="_blank">Section 5.7.2 of RFC 7230</a>. <a href="http://tools.ietf.org/html/rfc7234#section-5.2.2.4" target="_blank"><code>no-transform</code></a> parameter of <code>Cache-Control</code> header.', TEXTDOMAIN )
-			)
-			, new WPF\GUI\Setting\Page\Control\Input(
-				'vary'
-				, VARY
-				, __( 'Second cache keys', TEXTDOMAIN )
-				, __( 'List of request headers, for different values of which responses may be different. <a href="http://tools.ietf.org/html/rfc7234#section-4.1" target="_blank"><code>Vary</code></a> header.', TEXTDOMAIN )
-			)
+			, new WPF\GUI\Setting\Page\Control\Input( 'no_cache', NO_CACHE, array(
+				'title' => __( 'Disable cache for headers', TEXTDOMAIN )
+				, 'description' => __( 'Disable cache for specified HTTP headers, but enable cache for other parts of response. <code>*</code> - fully disable server, client (browser) and proxy servers cache, <code>""</code> - don\'t disable cache. <a href="http://tools.ietf.org/html/rfc7234#section-5.2.2.2" target="_blank"><code>no-cache</code></a> parameter of <code>Cache-Control</code> header.', TEXTDOMAIN )
+			) )
+			, new WPF\GUI\Setting\Page\Control\CheckBox( 'no_store', NO_STORE, array(
+				'title' => __( 'Disable permanent cache storage', TEXTDOMAIN )
+				, 'description' => __( 'Disable permanent cache storage (for security reasons) - <a href="http://tools.ietf.org/html/rfc7234#section-5.2.2.3" target="_blank"><code>no-store</code></a> parameter of <code>Cache-Control</code> header.', TEXTDOMAIN )
+			) )
+			, new WPF\GUI\Setting\Page\Control\CheckBox( 'public', CACHE_PUBLIC, array(
+				'title' => __( 'Enable public cache', TEXTDOMAIN )
+				, 'description' => __( 'Enable public cache (client (browser) and proxy servers cache) - <a href="http://tools.ietf.org/html/rfc7234#section-5.2.2.5" target="_blank"><code>public</code></a> parameter of <code>Cache-Control</code> header.', TEXTDOMAIN )
+			) )
+			, new WPF\GUI\Setting\Page\Control\Input( 'private', CACHE_PRIVATE, array(
+				'title' => __( 'Disable public cache for headers', TEXTDOMAIN )
+				, 'description' => __( 'Disable public cache for specified HTTP headers, but don\'t disable public cache for other parts of response. <code>*</code> - fully disable public (proxy servers) cache, <code>""</code> - don\'t disable public cache. <a href="http://tools.ietf.org/html/rfc7234#section-5.2.2.6" target="_blank"><code>private</code></a> parameter of <code>Cache-Control</code> header.', TEXTDOMAIN )
+			) )
+			, new WPF\GUI\Setting\Page\Control\Number( 'max_age', MAX_AGE, array(
+				'title' => __( 'Default <code>max-age</code> value', TEXTDOMAIN )
+				, 'postfix' => __( 'seconds', TEXTDOMAIN )
+				, 'description' => __( 'Default <a href="http://tools.ietf.org/html/rfc7234#section-5.2.2.8" target="_blank"><code>max-age</code></a> value (max cache time-to-live, seconds) for html wordpress pages.', TEXTDOMAIN )
+			) )
+			, new WPF\GUI\Setting\Page\Control\CheckBox( 'must_revalidate', MUST_REVALIDATE, array(
+				'title' => __( 'Doesn\'t use stale cache', TEXTDOMAIN )
+				, 'description' => __( 'If cache content to become stale, a cache <strong>must not</strong> use the response to satisfy requests without successful validation on the origin server. <a href="http://tools.ietf.org/html/rfc7234#section-5.2.2.1" target="_blank"><code>must-revalidate</code></a> parameter of <code>Cache-Control</code> header.', TEXTDOMAIN )
+			) )
+			, new WPF\GUI\Setting\Page\Control\CheckBox( 'proxy_revalidate', PROXY_REVALIDATE, array(
+				'title' => __( 'Doesn\'t use stale cache on proxy servers', TEXTDOMAIN )
+				, 'description' => __( 'If cache content on proxy servers to become stale, a cache <strong>must not</strong> use the response to satisfy requests without successful validation on the origin server. But private (browser) cache can use stale content. <a href="http://tools.ietf.org/html/rfc7234#section-5.2.2.7" target="_blank"><code>proxy-revalidate</code></a> parameter of <code>Cache-Control</code> header.', TEXTDOMAIN )
+			) )
+			, new WPF\GUI\Setting\Page\Control\CheckBox( 'no_transform', NO_TRANSFORM, array(
+				'title' => __( 'Don\'t transform content parts in cache', TEXTDOMAIN )
+				, 'description' => __( 'Cache (regardless of whether it implements a cache) <strong>must not</strong> transform the payload, as defined in <a href="http://tools.ietf.org/html/rfc7230#section-5.7.2" target="_blank">Section 5.7.2 of RFC 7230</a>. <a href="http://tools.ietf.org/html/rfc7234#section-5.2.2.4" target="_blank"><code>no-transform</code></a> parameter of <code>Cache-Control</code> header.', TEXTDOMAIN )
+			) )
+			, new WPF\GUI\Setting\Page\Control\Input( 'vary', VARY, array(
+				'title' => __( 'Second cache keys', TEXTDOMAIN )
+				, 'description' => __( 'List of request headers, for different values of which responses may be different. <a href="http://tools.ietf.org/html/rfc7234#section-4.1" target="_blank"><code>Vary</code></a> header.', TEXTDOMAIN )
+			) )
 		)
 		, new WPF\GUI\Setting\Page\Section\Base( 'rfc5861', __( 'RFC 5861 HTTP Cache-Control Extensions for Stale Content', TEXTDOMAIN )
-			, new WPF\GUI\Setting\Page\Control\Number(
-				'stale_while_revalidate'
-				, STALE_WHILE_REVALIDATE
-				, __( 'Return stale content while revalidate', TEXTDOMAIN )
-				, __( 'In seconds. A cached stale response <strong>may</strong> be used to satisfy the request, up to the indicated number of seconds, while source server request in progress. <a href="http://tools.ietf.org/html/rfc5861#section-3" target="_blank"><code>stale-while-revalidate</code></a> parameter of <code>Cache-Control</code> header.', TEXTDOMAIN )
-				, __( 'seconds', TEXTDOMAIN )
-			)
-			, new WPF\GUI\Setting\Page\Control\Number(
-				'stale_if_error'
-				, STALE_IF_ERROR
-				, __( 'Return stale content if error', TEXTDOMAIN )
-				, __( 'In seconds. When an error is encountered (in source server response), a cached stale response <strong>may</strong> be used to satisfy the request, regardless of other freshness information. <a href="http://tools.ietf.org/html/rfc5861#section-4" target="_blank"><code>stale-if-error</code></a> parameter of <code>Cache-Control</code> header.', TEXTDOMAIN )
-				, __( 'seconds', TEXTDOMAIN )
-			)
+			, new WPF\GUI\Setting\Page\Control\Number( 'stale_while_revalidate', STALE_WHILE_REVALIDATE, array(
+				'title' => __( 'Return stale content while revalidate', TEXTDOMAIN )
+				, 'postfix' => __( 'seconds', TEXTDOMAIN )
+				, 'description' => __( 'In seconds. A cached stale response <strong>may</strong> be used to satisfy the request, up to the indicated number of seconds, while source server request in progress. <a href="http://tools.ietf.org/html/rfc5861#section-3" target="_blank"><code>stale-while-revalidate</code></a> parameter of <code>Cache-Control</code> header.', TEXTDOMAIN )
+			) )
+			, new WPF\GUI\Setting\Page\Control\Number( 'stale_if_error', STALE_IF_ERROR, array(
+				'title' => __( 'Return stale content if error', TEXTDOMAIN )
+				, 'postfix' => __( 'seconds', TEXTDOMAIN )
+				, 'description' => __( 'In seconds. When an error is encountered (in source server response), a cached stale response <strong>may</strong> be used to satisfy the request, regardless of other freshness information. <a href="http://tools.ietf.org/html/rfc5861#section-4" target="_blank"><code>stale-if-error</code></a> parameter of <code>Cache-Control</code> header.', TEXTDOMAIN )
+			) )
 		)
 
 		, new WPF\GUI\Setting\Page\Component\Help\Base(
