dfe2fe49a2eeb7a3fa3982e715e2da8690e4410e
Sergey S. Betke <sergey.s.betke@yandex.ru>
2014-09-07 13:49:11 +0400

    + добавил заготовку второго плагина (плагина для указания отличных от общих параметров кэширования для отдельных страниц и разделов сайта) (#16)

--- _header-strings.php
+++ _header-strings.php
@@ -2,4 +2,6 @@
 __( 'ITG Cache-Control headers' );
 __( 'Automatically send HTTP 1.1 headers "Cache-control", "Pragma" and "Expires" (<a href="http://tools.ietf.org/html/rfc7234" target="_blank">RFC 7234</a>).' );
 __( 'Sergey S. Betke' );
+__( 'ITG Cache-Control headers - pages specific' );
+__( 'Add to "ITG Cache-Control headers" plugin posts and pages meta for posts specific cache parameters.' );
 ?>
